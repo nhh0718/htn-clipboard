cbb0c84bbd2e4fb6e2d7d04c22866566b54536dd
HoangNH <hoangnh0718@gmail.com>
2026-04-02 23:47:15 +0700

    fix: hide window to tray on close instead of quitting app

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -15,14 +16,19 @@ func main() {
 	app := NewApp(&assets)
 
 	err := wails.Run(&options.App{
-		Title:            "Clipboard Pro",
-		Width:            400,
-		Height:           600,
-		StartHidden:      false,
-		AlwaysOnTop:      false,
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		Title:             "Clipboard Pro",
+		Width:             400,
+		Height:            600,
+		StartHidden:       false,
+		HideWindowOnClose: true,
+		AlwaysOnTop:       false,
+		BackgroundColour:  &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		AssetServer: &assetserver.Options{
 			Assets: assets,
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
 		},
 		OnStartup:  app.startup,
 		OnShutdown: app.shutdown,
